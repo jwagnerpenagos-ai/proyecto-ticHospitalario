5004a1b9ef3e1979ff1224f84142290cf7b347a9
valentina230 <valentinapoveda01@gmail.com>
2026-05-27 22:53:14 -0500

    Add Dashboard changes and evidence

--- Proyecto Integrador 2026-1/auth_service/config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = "super_secret_key"
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:12345@db:5432/it_monitoring_db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
